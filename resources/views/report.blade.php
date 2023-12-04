952946fbb346074551303e85f3b9712528154b39
nithink13 <nithinnithu0486@gmail.com>
2023-12-04 23:12:07 +0530

    report page

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,42 +1,56 @@
 @extends('layouts.master')
 @section('content')
-<form>
-    @csrf
-    <input type="date" name="from date" id="" placeholder="From Date">
-    <input type="date" name="to date" id="" placeholder="To Date">
-    <select name="service_type" id="">
-        <option value="0" selected disabled>select service</option>
-        <option value="all">All</option>
-        <option value="payout">Payout</option>
-    </select>
-<button type="button" value="search_report">Search Report</button>
-</form>
-<table id="report_data" class="display nowrap" style="width:100%">
-        <thead>
-            <tr>
-                <th>Date - Time</th>
-                <th>Transaction ID</th>
-                <th>Mobile Number</th>
-                <th>Name</th>
-                <th>Bank Name</th>
-                <th>Account Number</th>
-                <th>Amount</th>
-                <th>Status</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>01-Dec-2023 - 12:24:32 PM</td>
-                <td>HFPTXaLie000012</td>
-                <td>6383224535</td>
-                <td>Hifi Money</td>
-                <td>HDFC Bank</td>
-                <td>2003020001020</td>
-                <td>₹12500</td>
-                <td>Success</td>
-                <td><button type="button"><i class="bi bi-printer-fill"></i></button><button type="button"><i class="bi bi-arrow-clockwise"></i></button></td>
-            </tr>
-        </tbody>
-    </table>
+<section style="margin-top: 94px;margin-bottom: 40px;padding: 0px 30px;">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-xs-12">
+                <div class="payout-box">
+                    <h4 style="float:left;">Report</h4>
+                    <form class="report-form">
+                        @csrf
+                        <input type="date" name="from date" id="" placeholder="From Date">
+                        <input type="date" name="to date" id="" placeholder="To Date">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Select Service</option>
+                            <option value="1">All</option>
+                            <option value="2">Payout</option>
+                        </select>
+                        <button class="btn" type="button" value="search_report">Search Report</button>
+                    </form>
+                    <table id="report_data" class="table display nowrap" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date - Time</th>
+                                <th scope="col">Transaction ID</th>
+                                <th scope="col">Mobile Number</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Bank Name</th>
+                                <th scope="col">Account Number</th>
+                                <th scope="col">Amount</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>01-Dec-2023 - 12:24:32 PM</td>
+                                <td>HFPTXaLie000012</td>
+                                <td>6383224535</td>
+                                <td>Hifi Money</td>
+                                <td>HDFC Bank</td>
+                                <td>2003020001020</td>
+                                <td>₹12500</td>
+                                <td>Success</td>
+                                <td><button class="print-btn" type="button"><i class="bi bi-printer-fill"></i></button>
+                                    <button class="print-btn" type="button"><i
+                                            class="bi bi-arrow-clockwise"></i></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
