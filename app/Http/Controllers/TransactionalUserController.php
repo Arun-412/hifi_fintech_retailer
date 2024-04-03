9013757c551208eccb39e805ce6b387478217b1a
Arun-412 <arunmozhi52892@gmail.com>
2024-04-03 06:51:16 +0530

    create customer base flow

--- app/Http/Controllers/TransactionalUserController.php
+++ app/Http/Controllers/TransactionalUserController.php
@@ -15,6 +15,54 @@ use DB;
 
 class TransactionalUserController extends Controller
 {
+    private $Base_URL;
+    private $Access_Key;
+    public function __construct() {
+        if(env("API_ACCESS_MODE") == "LIVE"){
+            $this->Base_URL = env("API_PRODUCTION_URL");
+            $this->Access_Key = env("API_PRODUCTION_ACCESS_KEY");
+        }else if(env("API_ACCESS_MODE") == "TEST"){
+            $this->Base_URL = env("API_STAGING_URL");
+            $this->Access_Key = env("API_STAGING_ACCESS_KEY");
+        }else{
+            $this->Base_URL = env("API_LOCAL_URL");
+            $this->Access_Key = env("API_LOCAL_ACCESS_KEY");         
+        }
+    }
+
+    public function curl_post($data) {
+        try{
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL =>  $this->Base_URL.$data['url'],
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_POST => true,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $data['data'],
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/x-www-form-urlencoded',
+                ),
+            ));
+            $responses = curl_exec($curl);
+            $err = curl_error($curl);
+            $response = 'Something went wrong from sending values for activation';
+            if ($err) {
+                $response = $err;
+            }else{
+                $response = $responses;
+            }
+            curl_close($curl);
+            return json_decode($response);
+        }catch(\Throwable $e){
+            return $e->getmessage();
+        }  
+    }
+
     public function user_login(Request $request){
         try{
             $validate = Validator::make($request->all(), [
@@ -40,27 +88,39 @@ class TransactionalUserController extends Controller
                     }
                 }
                 else{
-                    $user_access = transactional_user::create([
-                        'user_code' => "HFT".Str::random(4)."U".Str::random(4),
-                        'mobile_number' => $request->mobile_number,
-                        'created_by' => Auth::user()->door_code,
-                        'status' => "HFY",
-                    ]);
-                    if($user_access){
-                        $accounts_list = $this->user_accounts($user_code = $user_access->user_code);
-                        $data = array(
-                            "mobile"=>$request->mobile_number,
-                            "user"=>$user_access->user_code
-                        );
-                        if($accounts_list){
-                            $data['accounts'] = $accounts_list;
-                            return redirect('payout/dashboard')->with("success",$data);
-                        }   
-                        else{
-                            return redirect('payout/dashboard')->with("failed",$data);
+                    $data = array(
+                        "url"=>'create_customer',
+                        "data"=>
+                            'mobile='.$request->mobile_number
+                        ,
+                    );
+                    $customer = $this->curl_post($data);
+                    if($customer->status == true){
+                        $user_access = transactional_user::create([
+                            'user_code' => "HFT".Str::random(4)."U".Str::random(4),
+                            'mobile_number' => $request->mobile_number,
+                            'created_by' => Auth::user()->door_code,
+                            'status' => "HFY",
+                        ]);
+                        if($user_access){
+                            $accounts_list = $this->user_accounts($user_code = $user_access->user_code);
+                            $data = array(
+                                "mobile"=>$request->mobile_number,
+                                "user"=>$user_access->user_code
+                            );
+                            if($accounts_list){
+                                $data['accounts'] = $accounts_list;
+                                return redirect('payout/dashboard')->with("success",$data);
+                            }   
+                            else{
+                                return redirect('payout/dashboard')->with("failed",$data);
+                            }
+                        }else{
+                            return back()->with("failed","Unable to Register");
                         }
-                    }else{
-                        return back()->with("failed","Unable to Register");
+                    }
+                    else{
+                        return response()->json(['status'=>false,'message'=>$customer->message]);
                     }
                 }
             }
